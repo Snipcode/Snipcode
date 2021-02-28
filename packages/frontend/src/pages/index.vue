9b2becbb26ec3fafb857b519b5e95c5ee4450230
Mia <vottus@vott.us>
2021-02-28 22:50:52 +0100

    refactor: Use REST endpoints if Websockets are unavailable

--- packages/frontend/src/pages/index.vue
+++ packages/frontend/src/pages/index.vue
@@ -33,10 +33,10 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from '@nuxtjs/composition-api'
+import { defineComponent, reactive, useContext } from '@nuxtjs/composition-api'
 import { PasteDto } from '@pastte/backend/src/http/dto/db/pasteDto'
 import socketSend from '@pastte/backend/src/ws/helpers/socketSend'
-import { baseUrl } from '../api/axios'
+import { $axios, baseUrl } from '../api/axios'
 import { createEventEmitter } from '../api/ws/createEventEmitter'
 
 export default defineComponent({
@@ -45,21 +45,54 @@ export default defineComponent({
     const state = reactive({
       pastes: [] as PasteDto[],
       newPaste: '',
+      socket: null as any,
     })
 
-    const socket = new WebSocket(`${baseUrl(true)}/paste/websocket`)
+    const { store } = useContext()
 
-    const createPaste = () => {
+    // Loads pastes from User Store
+    state.pastes = store.getters['user/user'].pastes ?? []
+
+    // Creates the WebSocket connection
+    let socket: WebSocket | null = null
+    try {
+      if (!window.MSStream && /iPad|iPhone|iPod/.test(navigator.userAgent))
+        throw new Error('Websockets are fucked on iOS. Fuck you Apple.')
+      else socket = new WebSocket(`${baseUrl(true)}/paste/websocket`)
+    } catch (_) {
+      alert(
+        'Websocket connection failed. This website may not work as expected!'
+      )
+    }
+
+    // Creates a paste
+    const createPaste = async () => {
       try {
-        socketSend(socket, {
-          action: 'create_paste',
-          data: { content: state.newPaste },
-        })
+        if (socket)
+          socketSend(socket, {
+            action: 'create_paste',
+            data: { content: state.newPaste },
+          })
+        else {
+          const res = await $axios.request({
+            url: '/paste/create',
+            method: 'PUT',
+            data: {
+              data: {
+                content: state.newPaste,
+              },
+            },
+          })
+          if (!res.data.success) throw new Error('Failed to paste.')
+        }
+
         state.newPaste = ''
       } catch (_) {}
     }
 
+    // Ctrl+V event
     window.addEventListener('paste', async () => {
+      console.log('[client] paste event triggered')
       try {
         const paste = await navigator.clipboard.readText()
         if (!paste) return
@@ -69,24 +102,24 @@ export default defineComponent({
       } catch (_) {}
     })
 
-    socket.addEventListener('open', () => {
-      socketSend(socket, { action: 'fetch_pastes' })
-    })
-
-    const emitter = createEventEmitter(socket)
-
-    emitter.on('fetch_pastes', (msg) => {
-      state.pastes = msg.data.pastes
-    })
-
-    emitter.on('paste_create', (msg) => {
-      state.pastes.push(msg.data.paste)
-    })
+    // Add event listeners and create an emitter if
+    // WebSocket connection is made
+    if (socket) {
+      socket.addEventListener('open', () => {
+        socketSend(socket!, { action: 'fetch_pastes' })
+      })
+      const emitter = createEventEmitter(socket)
+      emitter.on('fetch_pastes', (msg) => {
+        state.pastes = msg.data.pastes
+      })
+      emitter.on('paste_create', (msg) => {
+        state.pastes.push(msg.data.paste)
+      })
+    }
 
     return {
       state,
       createPaste,
-      JSON,
     }
   },
 })
