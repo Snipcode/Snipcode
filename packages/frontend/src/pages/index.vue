54b1c22a79b9ad293de685c1d47de233c25a05a2
Mia <vottus@vott.us>
2021-03-04 00:57:23 +0100

    feat(frontend): redesign, deleting pastes, view paste

--- packages/frontend/src/pages/index.vue
+++ packages/frontend/src/pages/index.vue
@@ -1,31 +1,90 @@
 <template>
   <div>
-    <form class="pt-4 pl-2 border-2 border-purple-400 rounded-2xl">
-      <div class="pr-2">
+    <form>
+      <div class="pr-2 flex text-console font-mono gap-x-2 text-xs">
+        >
         <textarea
           v-model="state.newPaste"
           type="text"
-          class="w-full outline-none resize-none"
+          class="bg-transparent w-full outline-none resize-none"
           placeholder="Enter your paste..."
         />
       </div>
-      <div class="cursor-pointer flex justify-end">
-        <div
-          class="px-4 py-2 border-l-2 border-t-2 border-purple-400 rounded-tl-2xl rounded-br-2xl text-right hover:bg-gray-100"
-          @click.prevent="createPaste"
+
+      <div class="mb-6 flex text-console font-mono gap-x-2 text-xs">
         >
-          Pastte
+        <div class="flex gap-x-4">
+          <div
+            class="border-b mx-1 text-console font-mono border-console text-right hover:font-bold cursor-pointer uppercase"
+            @click.prevent="createPaste"
+          >
+            Submit
+          </div>
+          <div
+            class="border-b mx-1 text-console font-mono border-console text-right hover:font-bold cursor-pointer uppercase"
+            @click.prevent="createPaste"
+          >
+            Editor
+          </div>
+        </div>
+      </div>
+
+      <div class="mb-2 flex text-console font-mono gap-x-2 text-xs">
+        >
+        <div class="flex gap-x-4">
+          <div
+            class="border-b mx-1 text-console font-mono border-console text-right hover:font-bold cursor-pointer uppercase"
+            @click.prevent="state.showPastes = !state.showPastes"
+          >
+            Your Pastes
+          </div>
+          <div
+            class="border-b mx-1 text-console font-mono border-console text-right hover:font-bold cursor-pointer uppercase"
+          >
+            Logout
+          </div>
         </div>
       </div>
     </form>
 
     <div class="mt-6">
-      <div v-for="(paste, i) in state.pastes" :key="i">
-        <div
-          v-if="paste"
-          class="py-4 px-2 border-2 border-purple-400 rounded-2xl mb-2"
-        >
-          {{ paste.content }}
+      <div v-if="state.showPastes" class="overflow-auto max-h-36">
+        <div v-for="(paste, i) in state.pastes" :key="i">
+          <nuxt-link
+            :to="`/${paste.id}`"
+            class="flex justify-between py-1 px-2 mr-2 border-2 border-double border-console mb-2 text-console text-xs font-mono group cursor-pointer"
+          >
+            <div class="flex gap-x-2">
+              <span class="hidden group-hover:flex">></span>
+              <div>
+                {{ truncate(paste.content) }}
+
+                <div class="mt-1 text-xss">
+                  {{ new Date(paste.createdAt).toLocaleString() }}
+                </div>
+              </div>
+            </div>
+
+            <div
+              class="flex items-center hover:bg-console hover:text-white"
+              @click.prevent="() => removePaste({ id: paste.id })"
+            >
+              <svg
+                class="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1"
+                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                ></path>
+              </svg>
+            </div>
+          </nuxt-link>
         </div>
       </div>
     </div>
@@ -36,7 +95,8 @@
 import { defineComponent, reactive, useContext } from '@nuxtjs/composition-api'
 import { PasteDto } from '@pastte/backend/src/http/dto/db/pasteDto'
 import socketSend from '@pastte/backend/src/ws/helpers/socketSend'
-import { $axios, baseUrl } from '../api/axios'
+import { create as newPaste, remove as removePaste } from '../api/paste'
+import { baseUrl } from '../api/axios'
 import { createEventEmitter } from '../api/ws/createEventEmitter'
 
 export default defineComponent({
@@ -45,6 +105,7 @@ export default defineComponent({
     const state = reactive({
       pastes: [] as PasteDto[],
       newPaste: '',
+      showPastes: true,
       socket: null as any,
     })
 
@@ -74,15 +135,7 @@ export default defineComponent({
             data: { content: state.newPaste },
           })
         else {
-          const res = await $axios.request({
-            url: '/paste/create',
-            method: 'PUT',
-            data: {
-              data: {
-                content: state.newPaste,
-              },
-            },
-          })
+          const res = await newPaste({ content: state.newPaste })
           if (!res.data.success) throw new Error('Failed to paste.')
         }
 
@@ -113,13 +166,15 @@ export default defineComponent({
         state.pastes = msg.data.pastes
       })
       emitter.on('paste_create', (msg) => {
-        state.pastes.push(msg.data.paste)
+        state.pastes = [msg.data.paste, ...state.pastes]
       })
     }
 
     return {
       state,
       createPaste,
+      removePaste,
+      truncate: (str: string) => str.substring(0, 35),
     }
   },
 })
