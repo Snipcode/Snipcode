0370c507319c2305b53e4dcb0950d32bad901def
Mia <vottus@vott.us>
2021-03-07 04:32:56 +0100

    feat: REST endpoint to use invite

--- packages/backend/src/http/controllers/userController.ts
+++ packages/backend/src/http/controllers/userController.ts
@@ -1,6 +1,6 @@
 import { Controller } from '../../types'
 import { withUserContext } from '../context/userContext'
-import { success } from '../helpers/responseHelper'
+import { error, ErrorKind, success } from '../helpers/responseHelper'
 
 const UserController: Controller = async (app, { db }) => {
   app.get('/', async (req, res) =>
@@ -12,6 +12,73 @@ const UserController: Controller = async (app, { db }) => {
       },
       ({ dtoUser }) => res.send(success({ user: dtoUser }))
     )
+  )
+
+  app.post<{ Body: { data: { code: string } } }>(
+    '/invite',
+    {
+      schema: {
+        body: {
+          type: 'object',
+          required: ['data'],
+          properties: {
+            data: {
+              type: 'object',
+              required: ['code'],
+              properties: { code: { type: 'string' } },
+            },
+          },
+        },
+      },
+    },
+    async (req, res) =>
+      withUserContext({ req, res, deps: { db } }, async ({ user }) => {
+        if (user.invite)
+          return res.send(
+            error({
+              kind: ErrorKind.USER_INPUT,
+              message: 'You are already invited.',
+            })
+          )
+
+        const code = await db.invite.findUnique({
+          where: { id: req.body.data.code },
+        })
+        if (!code)
+          return res.send(
+            error({
+              kind: ErrorKind.USER_INPUT,
+              message: 'Invalid invite code',
+            })
+          )
+
+        if (code.takenById)
+          return res.send(
+            error({
+              kind: ErrorKind.USER_INPUT,
+              message: 'This code is already taken',
+            })
+          )
+
+        await db.user.update({
+          where: {
+            id: user.id,
+          },
+          data: {
+            invite: {
+              connect: {
+                id: code.id,
+              },
+            },
+          },
+        })
+
+        res.send(
+          success({
+            message: 'success',
+          })
+        )
+      })
   )
 }
 
