01468b0e50c6453324d80f5cc61309317528df98
Mia <vottus@vott.us>
2021-03-07 17:51:32 +0100

    feat(frontend): invite page

--- packages/backend/src/http/controllers/userController.ts
+++ packages/backend/src/http/controllers/userController.ts
@@ -1,6 +1,7 @@
 import { Controller } from '../../types'
 import { withUserContext } from '../context/userContext'
 import { error, ErrorKind, success } from '../helpers/responseHelper'
+import { User } from '../schemas'
 
 const UserController: Controller = async (app, { db }) => {
   app.get('/', async (req, res) =>
@@ -14,78 +15,55 @@ const UserController: Controller = async (app, { db }) => {
     )
   )
 
-  app.post<{ Body: { data: { code: string } } }>(
-    '/invite',
-    {
-      schema: {
-        body: {
-          type: 'object',
-          required: ['data'],
-          properties: {
-            data: {
-              type: 'object',
-              required: ['code'],
-              properties: {
-                code: {
-                  type: 'string',
-                  transform: ['trim'],
-                  minLength: 1,
-                },
-              },
-            },
-          },
-        },
-      },
-    },
-    async (req, res) =>
-      withUserContext({ req, res, deps: { db } }, async ({ user }) => {
-        if (user.invite)
-          return res.send(
-            error({
-              kind: ErrorKind.USER_INPUT,
-              message: 'You are already invited.',
-            })
-          )
-
-        const code = await db.invite.findUnique({
-          where: { id: req.body.data.code },
-        })
-        if (!code)
-          return res.send(
-            error({
-              kind: ErrorKind.USER_INPUT,
-              message: 'Invalid invite code',
-            })
-          )
-
-        if (code.takenById)
-          return res.send(
-            error({
-              kind: ErrorKind.USER_INPUT,
-              message: 'This code is already taken',
-            })
-          )
-
-        await db.user.update({
-          where: {
-            id: user.id,
-          },
-          data: {
-            invite: {
-              connect: {
-                id: code.id,
-              },
-            },
-            invites: { create: Array(10).fill({}) },
-          },
-        })
-
-        res.send(
-          success({
-            message: 'success',
+  app.post<User.Invite>('/invite', { schema: User.invite }, async (req, res) =>
+    withUserContext({ req, res, deps: { db } }, async ({ user }) => {
+      if (user.invite)
+        return res.send(
+          error({
+            kind: ErrorKind.USER_INPUT,
+            message: 'You are already invited.',
           })
         )
+
+      const code = await db.invite.findUnique({
+        where: { id: req.body.data.code },
       })
+      if (!code)
+        return res.send(
+          error({
+            kind: ErrorKind.USER_INPUT,
+            message: 'Invalid invite code',
+          })
+        )
+
+      if (code.takenById)
+        return res.send(
+          error({
+            kind: ErrorKind.USER_INPUT,
+            message: 'This code is already taken',
+          })
+        )
+
+      await db.user.update({
+        where: {
+          id: user.id,
+        },
+        data: {
+          invite: {
+            connect: {
+              id: code.id,
+            },
+          },
+          invites: { create: Array(10).fill({}) },
+        },
+      })
+
+      res.send(
+        success({
+          message: 'success',
+        })
+      )
+    })
   )
 }
 
