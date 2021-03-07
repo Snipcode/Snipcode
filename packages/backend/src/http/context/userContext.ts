fb7c8915835e2fd2a446d16961122178745b7ac9
Mia <vottus@vott.us>
2021-03-07 17:07:34 +0100

    feat(backend): withOptionalUserContext

--- packages/backend/src/http/context/userContext.ts
+++ packages/backend/src/http/context/userContext.ts
@@ -80,17 +80,31 @@ const createUserContext = async ({
  * @param {UseUserContext} deps The required dependencies for UserContext
  */
 const withUserContext = async (
-  { req, res, deps: { db } }: UseUserContext,
+  ctx: UseUserContext,
   fn: (userCtx: UserContext) => Promise<unknown> | unknown,
   errFn?: () => unknown
 ) => {
-  const context = await createUserContext({ req, deps: { db } })
+  const context = await createUserContext(ctx)
   if (!context.success) {
-    if (res) return res.send(context)
+    if (ctx.res) return ctx.res.send(context)
     return errFn?.()
   }
 
   return fn(context)
 }
 
-export { UseUserContext, UserContext, withUserContext, createUserContext }
+const withOptionalUserContext = async (
+  ctx: UseUserContext,
+  fn: (userCtx: UserContext | null) => Promise<unknown> | unknown
+) => {
+  const context = await createUserContext(ctx)
+  return fn(context.success ? context : null)
+}
+
+export {
+  UseUserContext,
+  UserContext,
+  withUserContext,
+  withOptionalUserContext,
+  createUserContext,
+}
